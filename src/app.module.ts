b5438df414fd506bcf9e41182f41fc2a99cd63f2
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-10-10 08:46:06 +0200

    Integrated MongooseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,34 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    // MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const logger = new Logger('MongooseModule');
+
+        return {
+          uri: configService.get<string>('MONGO_URI'),
+          connectionFactory: (connection) => {
+            connection.once('open', () => {
+              logger.log('MongoDB Connected');
+            });
+
+            connection.on('error', (err) => {
+              logger.error(`MongoDB Connection Error: ${err}`);
+            });
+            return connection;
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'],
+  });
+  const logger = new Logger('Bootstrap');
   await app.listen(3300);
+
+
+  logger.log('Application is running on: http://localhost:3300');
 }
 bootstrap();
